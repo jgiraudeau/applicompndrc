c842f1ab9262211199b9424666ff71d8307719d2
Jacques Giraudeau <imac2jacques@imac.home>
2026-01-28 17:59:12 +0100

    Force clean CORS allow-all

--- backend/main.py
+++ backend/main.py
@@ -108,7 +108,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # TEMPORARY RESTORE: Fix Failed to Fetch
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
